c4bf1cc473110af1de267f1ab55ab00308e2a02a
sofisl <55454395+sofisl@users.noreply.github.com>
2022-10-12 18:53:20 -0700

    build: update README for deprecation notice (#419)
    * build: update README for deprecation notice and delete all files except samples
    
    * Update owlbot.py
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- owlbot.py
+++ owlbot.py
@@ -1,6 +1,6 @@
-# Copyright 2021 Google LLC
+# Copyright 2022 Google LLC
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the License);
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
@@ -14,4 +14,6 @@
 
 import synthtool.languages.node as node
 
-node.owlbot_main(templates_excludes=[".github/release-please.yml"])
+node.owlbot_main(templates_excludes=[
+'README.md'
+])
